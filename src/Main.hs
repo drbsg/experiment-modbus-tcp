92ab134d433022063cc8cb17167c4da624cc16a4
Brett Gibson <315260+drbsg@users.noreply.github.com>
2018-04-05 22:38:55 +0100

    Make the register addresses configurable from the command-line.

--- src/Main.hs
+++ src/Main.hs
@@ -3,6 +3,7 @@ module Main where
 import Control.Exception (bracket)
 import Control.Monad.Trans.Except
 import Data.Semigroup ((<>))
+import GHC.Word (Word16)
 import Options.Applicative
 import System.Modbus.TCP
 import Network.Socket hiding (recv, send)
@@ -11,6 +12,8 @@ import Network.Socket.ByteString (recv, send)
 
 data Options = Options { optHost :: String
                        , optPort :: Int
+                       , optCount :: Word16
+                       , optStart :: Word16
                        } deriving (Show)
 
 options :: Parser Options
@@ -27,6 +30,14 @@ options = Options
                     showDefault <>
                     value 502 <>
                     help "Port number to use." )
+  <*> option auto ( long "count" <>
+                    short 'c' <>
+                    metavar "COUNT" <>
+                    showDefault <>
+                    value 1 <>
+                    help "Number of registers to read." )
+  <*> argument auto ( metavar "REGISTER" <>
+                      help "Address of first register to read." )
 
 main :: IO ()
 main = runRequest =<< execParser opts
@@ -35,7 +46,7 @@ main = runRequest =<< execParser opts
           progDesc "Read some holding registers from the given device." )
 
 runRequest :: Options -> IO ()
-runRequest (Options host port) = withSocketsDo $ do
+runRequest (Options host port count register) = withSocketsDo $ do
   addr <- resolve host (show port)
   bracket (open addr) close go
 
@@ -56,7 +67,7 @@ runRequest (Options host port) = withSocketsDo $ do
                             , connCommandTimeout = 1000
                             , connRetryWhen = \_ _ -> False
                             }
-      result <- runExceptT $ runSession conn $ readHoldingRegisters (TransactionId 1) (ProtocolId 0) (UnitId 0) 1 3
+      result <- runExceptT $ runSession conn $ readHoldingRegisters (TransactionId 1) (ProtocolId 0) (UnitId 0) (RegAddress register) count
       case result of
         Left err -> putStrLn $ show err
         Right registers -> do
